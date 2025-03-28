36e7e399d3a8e8b94a4a087dc17dafedc811e3d6
slejeune <sebastien.lejeune@thalesgroup.com>
2025-03-28 08:57:54 +0100

    Same procedure on Gitlab deployment

--- scripts/ggi_deploy_gitlab.py
+++ scripts/ggi_deploy_gitlab.py
@@ -2,22 +2,20 @@
 # ######################################################################
 # Copyright (c) 2022 Boris Baldassari, Nico Toussaint and others
 #
-# This program and the accompanying materials are made
-# available under the terms of the Eclipse Public License 2.0
+# This program and the accompanying materials are made available under the terms of the Eclipse Public License 2.0
 # which is available at https://www.eclipse.org/legal/epl-2.0/
 #
 # SPDX-License-Identifier: EPL-2.0
 ######################################################################
 
 """
-
+Deploy script for GitLab.
 """
+
 import urllib.parse
-
 import gitlab
-
 from ggi_deploy import *
-
+from ggi_utils_gitlab import retrieve_params
 
 def main():
     """
@@ -26,11 +24,11 @@ def main():
     args = parse_args()
 
     print("* Using GitLab backend.")
-    metadata, params, init_scorecard = retrieve_env()
+    metadata, init_scorecard = retrieve_env()
+    params = retrieve_params()
     setup_gitlab(metadata, params, init_scorecard, args)
 
     print("\nDone.")
-
 
 def create_gitlab_label(project, existing_labels, new_label, label_args):
     """
@@ -42,72 +40,22 @@ def create_gitlab_label(project, existing_labels, new_label, label_args):
         print(f" Create label: {new_label}")
         project.labels.create(label_args)
 
-
 def setup_gitlab(metadata, params: dict, init_scorecard, args: dict):
     """
-    Executes the following deployment sequence on a GitLab instance:
-    * Reads gitlab-specific variables.
-    * Connect to GitLab
-    * Create labels & activities
-    * Create Goals board
-    * Create schedule for pipeline
+    Executes the deployment on a GitLab instance.
     """
 
-    # Get conf: URL
-    if 'CI_SERVER_URL' in os.environ:
-        params['gitlab_url'] = os.environ['CI_SERVER_URL']
-        print("- Using URL from env var 'CI_SERVER_URL'")
-    elif 'GGI_GITLAB_URL' in os.environ:
-        params['gitlab_url'] = os.environ['GGI_GITLAB_URL']
-        print("- Using URL from env var 'GGI_GITLAB_URL'")
-    elif 'gitlab_url' in params :
-        print("- Using URL from configuration file")
-    else:
-        print("Cannot find GitLab root URL, Exiting.")
-        exit(1)
-
-    # Get conf: Project
-    if 'CI_PROJECT_PATH' in os.environ:
-        params['gitlab_project'] = os.environ['CI_PROJECT_PATH']
-        print("- Using Project from env var 'CI_PROJECT_PATH'")
-    elif 'GGI_GITLAB_PROJECT' in os.environ:
-        params['gitlab_project'] = os.environ['GGI_GITLAB_PROJECT']
-        print("- Using Project from env var 'GGI_GITLAB_PROJECT'")
-    elif 'gitlab_project' in params:
-        print(f"- Using Project from configuration file")
-    else:
-        print("Cannot find GitLab project (org + repo), e.g. ospo-alliance/" +
-              "my-ggi-board. Exiting.")
-        exit(1)
-
-    if 'GGI_GITLAB_TOKEN' in os.environ:
-        print("- Using token from env var 'GGI_GITLAB_TOKEN'")
-        params['gitlab_token'] = os.environ['GGI_GITLAB_TOKEN']
-    else:
-        print("- Cannot find env var GGI_GITLAB_TOKEN. Please set it and re-run me.")
-        exit(1)
-
-    params['gitlab_repo_url'] = urllib.parse.urljoin(params['gitlab_url'], params['gitlab_project'])
-    params['gitlab_activities_url'] = urllib.parse.urljoin(params['gitlab_repo_url'], '/', '-/boards')
-    print("Configuration:")
-    print("URL       : " + params['gitlab_url'])
-    print("Project   : " + params['gitlab_project'])
-    print("Full URL  : " + params['gitlab_repo_url'])
-    print("Activities: " + params['gitlab_activities_url'])
-
-    print(f"\n# Connection to GitLab at {params['gitlab_url']} ")
-    gl = gitlab.Gitlab(url=params['gitlab_url'],
-                       per_page=50,
-                       private_token=params['gitlab_token'])
-    project = gl.projects.get(params['gitlab_project'])
+    print(f"\n# Connection to GitLab at {params['GGI_GITLAB_URL']} ")
+    gl = gitlab.Gitlab(url=params['GGI_GITLAB_URL'], per_page=50, private_token=params['GGI_GITLAB_TOKEN'])
+    project = gl.projects.get(params['GGI_GITLAB_PROJECT'])
 
     # Update current project description with Website URL
     if args.opt_projdesc:
         print("\n# Update Project description")
         if 'CI_PAGES_URL' in os.environ:
-            ggi_activities_url = params['gitlab_activities_url']
+            ggi_activities_url = params['GGI_ACTIVITIES_URL']
             ggi_handbook_version = metadata['handbook_version']
-            ggi_pages_url = os.environ['CI_PAGES_URL']
+            ggi_pages_url = params['GGI_PAGES_URL']
             desc = (
                 'Your own Good Governance Initiative project.\n\n'
                 'Here you will find '
@@ -124,40 +72,24 @@ def setup_gitlab(metadata, params: dict, init_scorecard, args: dict):
         else:
             print("Cannot find environment variable 'CI_PAGES_URL', skipping.")
 
-    #
     # Create labels & activities
-    #
     if args.opt_activities:
-
         print("\n# Manage labels")
         existing_labels = [i.name for i in project.labels.list()]
 
-        # Create role labels if needed
         print("\n Roles labels")
         for label, colour in metadata['roles'].items():
             create_gitlab_label(project, existing_labels, label, {'name': label, 'color': colour})
 
-        # Create labels for activity tracking
         print("\n Progress labels")
         for name, label in params['progress_labels'].items():
             create_gitlab_label(project, existing_labels, label, {'name': label, 'color': '#ed9121'})
 
-        # Create goal labels if needed
         print("\n Goal labels")
         for goal in metadata['goals']:
-            create_gitlab_label(project, existing_labels, goal['name'],
-                         {'name': goal['name'], 'color': goal['colour']})
+            create_gitlab_label(project, existing_labels, goal['name'], {'name': goal['name'], 'color': goal['colour']})
 
-        # # Read the custom scorecard init file.
-        # print(f"\n# Reading scorecard init file from {init_scorecard_file}.")
-        # with open(init_scorecard_file, 'r', encoding='utf-8') as f:
-        #     init_scorecard = f.readlines()
-
-        # Create issues with their associated labels.
         print("\n# Create activities.")
-        # First test the existence of Activities Issues:
-        #   if at least one Issue is found bearing one Goal label,
-        #   consider that all Issues exist and do not add any.
         issues_test = project.issues.list(state='opened')
         if len(issues_test) > 0:
             print(" Ignore, Issues already exist")
@@ -165,53 +97,34 @@ def setup_gitlab(metadata, params: dict, init_scorecard, args: dict):
             for activity in metadata['activities']:
                 progress_label = params['progress_labels']['not_started']
                 if args.opt_random:
-                    # randomly choose among valid progress labels
-                    # + artificially introduce an extra option for no progress label
                     progress_idx = random.choice(list(params['progress_labels']) + ['none'])
                     if progress_idx != 'none':
                         progress_label = params['progress_labels'][progress_idx]
-                labels = \
-                    [activity['goal']] + \
-                    activity['roles'] + \
-                    [progress_label]
+                labels = [activity['goal']] + activity['roles'] + [progress_label]
                 print(f"  - Issue: {activity['name']:<60} Labels: {labels}")
                 ret = project.issues.create({'title': activity['name'],
                                              'description': extract_sections(args, init_scorecard, activity),
                                              'labels': labels})
 
-    #
     # Create Goals board
-    #
     if args.opt_board:
         print(f"\n# Create Goals board: {ggi_board_name}")
-        boards_list=project.boards.list()
-        board_exists=False
-        for b in boards_list:
-            if b.name == ggi_board_name:
-                board_exists=True
-                break
+        boards_list = project.boards.list()
+        board_exists = any(b.name == ggi_board_name for b in boards_list)
         if board_exists:
             print(" Ignore, Board already exists")
         else:
             board = project.boards.create({'name': ggi_board_name})
             print('\n# Create Goals board lists.')
-            # First build an ordered list of goals
-            goal_lists = []
-            for g in metadata['goals']:
-                for l in project.labels.list():
-                    if l.name == g['name']:
-                        goal_lists.append(l)
-                        break
-
-            # Then create the lists in gitlab
+            goal_lists = [l for g in metadata['goals'] for l in project.labels.list() if l.name == g['name']]
             for goal_label in goal_lists:
                 print(f"  - Create list for {goal_label.name}")
                 b_list = board.lists.create({'label_id': goal_label.id})
 
-    # Create a scheduled pipeline trigger, if none exist yet.
+    # Schedule nightly pipeline
     if args.opt_schedulepipeline:
         print(f"\n# Schedule nightly pipeline to refresh the Dashboard")
-        nb_pipelines=len(project.pipelineschedules.list())
+        nb_pipelines = len(project.pipelineschedules.list())
         if nb_pipelines > 0:
             print(f" Ignore, already {nb_pipelines} scheduled pipeline(s)")
         else:
@@ -220,6 +133,7 @@ def setup_gitlab(metadata, params: dict, init_scorecard, args: dict):
                 'description': 'Nightly Update',
                 'cron': '0 3 * * *'})
             print(f" Pipeline created: '{sched.description}'")
+
 
 if __name__ == '__main__':
     main()
---
+++ scripts/ggi_utils_gitlab.py
@@ -0,0 +1,103 @@
+#!/usr/bin/python3
+# ######################################################################
+# Copyright (c) 2025 The OSPO Alliance contributors
+#
+# This program and the accompanying materials are made available
+# under the terms of the Eclipse Public License 2.0
+# which is available at https://www.eclipse.org/legal/epl-2.0/
+#
+# SPDX-License-Identifier: EPL-2.0
+######################################################################
+
+"""
+GitLab utilities for GGI tools.
+"""
+
+import json
+import os
+import urllib.parse
+import tldextract
+
+from ggi_deploy import *
+
+def retrieve_params():
+    """
+    Read metadata for activities and deployment options.
+
+    Determine GitLab server URL and Project name:
+    * From environment variables if available, or
+    * From configuration file otherwise.
+    """
+
+    print(f"# Reading deployment options from {conf_file}.")
+    with open(conf_file, 'r', encoding='utf-8') as f:
+        params = json.load(f)
+
+    # GitLab URL
+    if 'CI_SERVER_URL' in os.environ:
+        params['GGI_GITLAB_URL'] = os.environ['CI_SERVER_URL']
+        print("- Using GitLab URL from env var 'CI_SERVER_URL'")
+    elif 'GGI_GITLAB_URL' in os.environ:
+        params['GGI_GITLAB_URL'] = os.environ['GGI_GITLAB_URL']
+        print("- Using GitLab URL from env var 'GGI_GITLAB_URL'")
+    elif 'gitlab_url' in params:
+        params['GGI_GITLAB_URL'] = params['gitlab_url']
+        print("- Using GitLab URL from configuration file")
+    else:
+        print("Cannot find GitLab URL. Exiting.")
+        exit(1)
+
+    # GitLab Project
+    if 'CI_PROJECT_PATH' in os.environ:
+        params['GGI_GITLAB_PROJECT'] = os.environ['CI_PROJECT_PATH']
+        print("- Using Project from env var 'CI_PROJECT_PATH'")
+    elif 'GGI_GITLAB_PROJECT' in os.environ:
+        params['GGI_GITLAB_PROJECT'] = os.environ['GGI_GITLAB_PROJECT']
+        print("- Using Project from env var 'GGI_GITLAB_PROJECT'")
+    elif 'gitlab_project' in params:
+        params['GGI_GITLAB_PROJECT'] = params['gitlab_project']
+        print("- Using Project from configuration file")
+    else:
+        print("Cannot find GitLab project (org/repo). Exiting.")
+        exit(1)
+
+    # GitLab Token
+    if 'GGI_GITLAB_TOKEN' in os.environ:
+        params['GGI_GITLAB_TOKEN'] = os.environ['GGI_GITLAB_TOKEN']
+        print("- Using token from env var 'GGI_GITLAB_TOKEN'")
+    else:
+        print("Cannot find env var 'GGI_GITLAB_TOKEN'. Exiting.")
+        exit(1)
+
+    # Pages URL
+    if 'CI_PAGES_URL' in os.environ:
+        params['GGI_PAGES_URL'] = os.environ['CI_PAGES_URL']
+        print("- Using Pages URL from env var 'CI_PAGES_URL'")
+    else:
+        print("- Pages URL not found in env. Computing fallback.")
+        pieces = tldextract.extract(params['GGI_GITLAB_URL'])
+        params['GGI_PAGES_URL'] = 'https://' + params['GGI_GITLAB_PROJECT'].split('/')[0] +                                   '.' + pieces.domain + '.io/' + params['GGI_GITLAB_PROJECT'].split('/')[-1]
+
+    # Compose URLs
+    params['GGI_URL'] = urllib.parse.urljoin(params['GGI_GITLAB_URL'], params['GGI_GITLAB_PROJECT'])
+    params['GGI_ACTIVITIES_URL'] = os.path.join(params['GGI_URL'] + '/', '-/boards')
+
+    print("Configuration:")
+    print("URL     : " + params['GGI_GITLAB_URL'])
+    print("Project : " + params['GGI_GITLAB_PROJECT'])
+    print("Full URL: " + params['GGI_URL'])
+
+    return params
+
+
+def main():
+    """
+    Test GitLab utils.
+    """
+    params = retrieve_params()
+    print("- GGI_GITLAB_PROJECT: " + params['GGI_GITLAB_PROJECT'])
+    print("- GGI_GITLAB_URL: " + params['GGI_GITLAB_URL'])
+    print("- GGI_PAGES_URL: " + params['GGI_PAGES_URL'])
+
+if __name__ == '__main__':
+    main()
